71de6460da1b6465dc9b307e750326cd6a34e6c9
Sarthak2004-coder <d4rkshad0w2004@gmail.com>
2025-04-26 16:18:23 +0530

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,14 @@
+def maxStolenValue(val):
+    n = len(val)
+    if n==0:
+        return 0
+    if n==1:
+        return val[0]
+    dp = [0]*n
+    dp[0]=val[0]
+    dp[1]=max(val[0], val[1])
+    for i in range(2, n):
+        dp[i]=max(val[i] + dp[i-2], dp[i-1])
+    return dp[-1]
+val = [6, 7, 1, 3, 8, 2, 5]
+print(maxStolenValue(val))
